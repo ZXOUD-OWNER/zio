c1d270e4e7aa8ed176c17d0020c80a12fceba188
chentiancan <2354739167@qq.com>
2024-06-19 14:11:02 +0800

    reset

--- zmqLoadBalancing/head/head.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "zmqMiddle.hpp"
-#include "log.hpp"
--- zmqLoadBalancing/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "head.hpp"
-
-int main(int argc, char **argv)
-{
-    Log_MQ mq(argv[0]);
-    zmqMiddle middle(Singleton::getInstance().getInstance().GetConf());
-    middle.start();
-    return 0;
-}
